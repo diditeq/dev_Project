662b6aaf7c6d23ced11856a0741800a46cfbff20
Doris Akpan <108197709+diditeq@users.noreply.github.com>
2024-09-07 08:45:44 +0100

    code commit

---
+++ tmsapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name='tasks')
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    priority = models.IntegerField(default=1)
+    description = models.TextField(default='')
+    location = models.CharField(max_length=255, default='')
+    organizer = models.CharField(max_length=100, default='')
+    is_completed = models.BooleanField(default=False)  # New field to track completion
